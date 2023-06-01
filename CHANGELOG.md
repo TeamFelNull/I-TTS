fc8de6e01002a848f46d049d0195ef501069c27a
MORIMORI0317 <morimori.0317@outlook.jp>
2023-06-01 20:29:33 +0900

    2.0.0-alpha.1

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# Changelog
+
+Changelog to track updates for this mod.  
+Add your changes to Unreleased if you want to commit.  
+Please write according to [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
+
+## [Unreleased]
+
+### Added
+
+- First Release
+
+### Changed
+
+### Deprecated
+
+### Removed
+
+### Fixed
+
+### Security
+
+[Unreleased]: https://github.com/TeamFelnull/I-TTS/commits
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,27 @@
+plugins {
+    id("org.jetbrains.changelog") version "2.0.0"
+}
+
+version = if (System.getenv("GITHUB_REF") != null && System.getenv("GITHUB_REF").startsWith("refs/tags/v")) {
+    System.getenv("GITHUB_REF").substring("refs/tags/v".length);
+} else {
+    "NONE"
+}
+
+changelog {
+    repositoryUrl.set("https://github.com/TeamFelnull/I-TTS")
+
+    introduction.set(
+        """
+    Changelog to track updates for this mod.  
+    Add your changes to Unreleased if you want to commit.  
+    Please write according to [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
+    """.trimIndent()
+    )
+
+    combinePreReleases.set(false)
+}
+
 allprojects {
     repositories {
         mavenCentral()
