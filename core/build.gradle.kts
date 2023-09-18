00d4ea603e8cab18567f748ce638d2d27326334e
MORIMORI0317 <morimori.0317@outlook.jp>
2023-09-19 01:18:48 +0900

    JDAとLavaPlayerを更新

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -13,13 +13,13 @@ checkstyle {
 
 dependencies {
     //https://github.com/DV8FromTheWorld/JDA/pull/2240 <-音声の遅れが生じる可能あり
-    api("net.dv8tion:JDA:5.0.0-beta.12")
+    api("net.dv8tion:JDA:5.0.0-beta.13")
     api("org.apache.commons:commons-lang3:3.12.0")
     api("com.google.code.gson:gson:2.10")
     api("com.google.guava:guava:31.1-jre")
     api("org.apache.logging.log4j:log4j-core:2.18.0")
     api("dev.felnull:felnull-java-library:1.75")
-    api("com.github.walkyst:lavaplayer-fork:1.3.99.2")
+    api("com.github.walkyst:lavaplayer-fork:1.4.3")
     api("commons-io:commons-io:2.11.0")
     api("com.ibm.icu:icu4j:72.1")
 
