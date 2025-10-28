51f65ed0787f436b9f3552bd758dab1ba60ea1be
MORIMORI0317 <morimori.0317@outlook.jp>
2025-10-28 21:32:03 +0900

    ログ出力改善

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -16,7 +16,6 @@ dependencies {
     api("org.apache.commons:commons-lang3:3.19.0")
     api("com.google.code.gson:gson:2.13.2")
     api("com.google.guava:guava:33.5.0-jre")
-    api("org.apache.logging.log4j:log4j-core:3.0.0-beta3")
     api("dev.felnull:felnull-java-library:1.75")
     api("dev.arbjerg:lavaplayer:2.2.4")
     api("commons-io:commons-io:2.20.0")
@@ -24,6 +23,10 @@ dependencies {
     api("com.atilika.kuromoji:kuromoji-ipadic:0.9.0")
 
     api("org.jetbrains:annotations:26.0.2-1")
+
+    api("org.slf4j:slf4j-api:2.1.0-alpha1")
+    api("org.apache.logging.log4j:log4j-core:3.0.0-beta2")
+    api("org.apache.logging.log4j:log4j-slf4j2-impl:3.0.0-beta2") // runtimeOnly
 }
 
 tasks.getByName<Test>("test") {
